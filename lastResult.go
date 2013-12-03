b40d3c2d915c8f215517b94971d90909945d743c
Friðrik Runólfsson <friddz@gmail.com>
2013-12-03 22:05:59 +0000

    added lastGameDrawn and lastGameLost, refactored find to a reusable function

--- lastResult.go
+++ lastResult.go
@@ -1,12 +1,25 @@
 package statmachine
 
-func lastWin(res []Result) Result {
-	win := Result{}
+func lastGameWon(res []Result) Result {
+	return findFirst(res, func(r Result) bool { return r.goals>r.opponentGoals})
+}
+
+func lastGameLost(res []Result) Result {
+	return findFirst(res, func(r Result) bool {return r.goals<r.opponentGoals})
+}
+
+func lastGameDrawn(res []Result) Result {
+	return findFirst(res, func(r Result) bool { return r.goals == r.opponentGoals})
+}
+
+func findFirst(res []Result, f func(Result) bool) Result {
+	found := Result{}
 	for _,e := range res {
-		if e.goals>e.opponentGoals {
-			win = e
-			break
+		if f(e){
+			found = e
+			break;
 		}
 	}
-	return win
+	return found
 }
+
--- lastResult_test.go
+++ lastResult_test.go
@@ -2,7 +2,7 @@ package statmachine
 
 import "testing"
 
-func TestLastWin(t *testing.T){
+func TestLastGameWon(t *testing.T){
 	allResults  := []Result{
 		Result{0,0,0},
 		Result{1,2,0},
@@ -10,9 +10,38 @@ func TestLastWin(t *testing.T){
 		Result{3,4,1},
 		}
 	
-	win := lastWin(allResults)
+	win := lastGameWon(allResults)
 	
 	if 1!=win.id{
-		t.Errorf("Didnt get correct win, got result with id%v", win.id)
+		t.Errorf("Didnt get correct last game won, got result with id %v", win.id)
+	}
+}
+
+func TestLastGameLost(t *testing.T){
+	allResults  := []Result{
+		Result{0,0,0},
+		Result{1,2,0},
+		Result{2,0,3},
+		Result{3,4,1},
+		}
+	
+	lost := lastGameLost(allResults)
+	
+	if 2!=lost.id{
+		t.Errorf("Didnt get correct last game lost, got result with id %v", lost.id)
+	}
+}
+
+func TestLastGameDrawn(t *testing.T){
+	allResults  := []Result{
+		Result{0,0,0},
+		Result{1,2,0},
+		Result{2,0,3},
+		Result{3,4,1},
+		}
+	drawn := lastGameDrawn(allResults)
+	
+	if 0!=drawn.id{
+		t.Errorf("Didnt get correct last game drawn, got result with id %v", drawn.id)
 	}
 }
