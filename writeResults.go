e7bc7ae19e1da5d3bd796ffdbfe3b82a5034242a
Friðrik Runólfsson <friddz@gmail.com>
2013-12-27 13:11:36 +0000

    Modified functions that return result info strings

--- writeResults.go
+++ writeResults.go
@@ -1,9 +1,15 @@
 package statmachine
 import "fmt"
 
-func writeResults(res []Result) string{
+func ResultInfoString(res []Result) string{
 	wins := gamesWon(res)
 	draws := gamesDrawn(res)
 	lost := gamesLost(res)
 	return fmt.Sprintf("Total (W-D-L): %v-%v-%v",wins, draws,lost)
+}
+
+func GoalInfoString(res []Result) string{
+	cleanSheets := cleanSheets(res)
+	gamesScoredIn := gamesScoredIn(res)
+	return fmt.Sprintf("Games Scored In/Clean Sheets: %v/%v", gamesScoredIn, cleanSheets)
 }
