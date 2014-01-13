eae652343ae78c2f2dcf0eb21ec26f6b6f64686e
Friðrik Runólfsson <friddz@gmail.com>
2014-01-13 22:29:26 +0000

    Refactored BiggestWins and BiggestLosses functions

--- resultRecords.go
+++ resultRecords.go
@@ -1,39 +1,27 @@
 package statmachine
 
 func BiggestWins(res []Result) []Result{
-	wins := make([] Result, 0)
-	maxWinBy :=0
-	for _,r :=range res{
-		if(isWin(r)){
-			scored :=goalsScored(r)
-			conceded :=goalsConceded(r)
-			if((scored-conceded)>maxWinBy){
-				maxWinBy = goalsScored(r)-goalsConceded(r)
-				wins = make([] Result, 0)
-				wins = append(wins, r)
-			}else if((scored-conceded)==maxWinBy){
-				wins = append(wins, r)
-			}
-		}
-	}
-	return wins
+	return findMaximizingResults(res, isWin, func(r Result) int{return goalsScored(r)-goalsConceded(r)})
 }
 
 func BiggestLosses(res []Result) []Result{
-	losses :=make([]Result, 0)
-	maxLossBy :=0
-	for _,r :=range res{
-		if(isLoss(r)){
-			scored :=goalsScored(r)
-			conceded :=goalsConceded(r)
-			if((conceded-scored)>maxLossBy){
-				maxLossBy = conceded-scored
-				losses = make([]Result,0)
-				losses = append(losses, r)
-			}else if((conceded-scored)==maxLossBy){
-				losses = append(losses, r)
+	return findMaximizingResults(res, isLoss, func(r Result) int {return goalsConceded(r)-goalsScored(r)})
+}
+
+func findMaximizingResults(res []Result, filter func(Result) bool, f func(Result) int) []Result{
+	results :=make([]Result, 0)
+	maxValue :=0
+	for _,r := range res{
+		if(filter(r)){
+			currentValue :=f(r)
+			if(currentValue>maxValue){
+				maxValue = currentValue
+				results = make([]Result, 0)
+				results = append(results, r)
+			}else if(currentValue == maxValue){
+				results = append(results, r)
 			}
 		}
 	}
-	return losses
+	return results
 }
