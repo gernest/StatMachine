e80c22d30fc23d5b384a618c15efa839d7f25f30
Friðrik Runólfsson <friddz@gmail.com>
2014-01-13 22:18:58 +0000

    added biggest losses function

--- resultRecords.go
+++ resultRecords.go
@@ -18,3 +18,22 @@ func BiggestWins(res []Result) []Result{
 	}
 	return wins
 }
+
+func BiggestLosses(res []Result) []Result{
+	losses :=make([]Result, 0)
+	maxLossBy :=0
+	for _,r :=range res{
+		if(isLoss(r)){
+			scored :=goalsScored(r)
+			conceded :=goalsConceded(r)
+			if((conceded-scored)>maxLossBy){
+				maxLossBy = conceded-scored
+				losses = make([]Result,0)
+				losses = append(losses, r)
+			}else if((conceded-scored)==maxLossBy){
+				losses = append(losses, r)
+			}
+		}
+	}
+	return losses
+}
--- resultRecords_test.go
+++ resultRecords_test.go
@@ -36,7 +36,7 @@ func TestBiggestWinIfNoWin(t *testing.T){
 	
 	wins := BiggestWins(allResults)
 	if(0!=len(wins)){
-		t.Errorf("Didnt get correct biggest wins, expected to not find any but found %v", len(wins))
+		t.Errorf("Didnt get correct biggest wins, expected not to find any but found %v", len(wins))
 	}
 }
 
@@ -53,5 +53,62 @@ func TestBiggestWinIfMultiple(t *testing.T){
 	wins := BiggestWins(allResults)
 	if(3!=len(wins)){
 		t.Errorf("Didnt get correct # of biggest wins, expected 3 but got %v", wins)
+	}
+	
+	for _,r := range wins{
+		if(goalsScored(r)-goalsConceded(r)!=3){
+			t.Errorf("Got a result with unexpected goal difference in TestBiggestWinIfMultiple, got %v, expected 3",goalsScored(r)-goalsConceded(r)) 
+		}
+	}
+}
+
+func TestBiggestLosses(t *testing.T){
+	allResults  := []Result{
+		NewResult(0,0,1,0,0,0,true,0, time.Now()),
+		NewResult(1,0,4,0,0,0,false,0, time.Now()),
+		NewResult(2,0,0,3,0,0,true,0, time.Now()),
+		NewResult(3,0,1,1,0,0,false,0, time.Now()),
+		NewResult(4,0,5,2,0,0,false,0, time.Now()),
+		NewResult(5,0,1,1,0,0,true,0, time.Now()),
+	}
+	
+	losses := BiggestLosses(allResults)
+	if(1!=len(losses)){
+		t.Errorf("Didnt get correct # of biggest losses, expected 1 but got %v", losses)
+	}
+	if(!(0==goalsScored(losses[0]) && 3==goalsConceded(losses[0]))){
+		t.Errorf("Didnt get correct biggest loss, got %v", losses[0])
+	}
+}
+
+func TestBiggestLossesIfNone(t *testing.T){
+	allResults :=[]Result{
+		NewResult(0,0,1,0,0,0,true,0, time.Now()),
+	}
+	
+	losses :=BiggestLosses(allResults)
+	if(0!=len(losses)){
+		t.Errorf("Didnt get correct biggest losses results, expected not to find any but found %v", len(losses))
+	}
+}
+func TestBiggestLossesIfMultiple(t *testing.T){
+	allResults  := []Result{
+		NewResult(0,0,0,1,0,0,true,0, time.Now()),
+		NewResult(1,0,0,1,0,0,false,0, time.Now()),
+		NewResult(2,0,0,2,0,0,true,0, time.Now()),
+		NewResult(3,0,1,3,0,0,false,0, time.Now()),
+		NewResult(4,0,0,0,0,0,false,0, time.Now()),
+		NewResult(5,0,0,0,0,0,true,0, time.Now()),
+	}
+	
+	losses :=BiggestLosses(allResults)
+	if(2!=len(losses)){
+		t.Errorf("Didnt get correct # of biggest losses, expected 2 but got %v", losses)
+	}
+	
+	for _,r := range losses{
+		if(goalsScored(r)-goalsConceded(r)!=-2){
+			t.Errorf("Got a result with unexpected goal difference in TestBiggestLossesIfMultiple, got %v, expected -2",goalsScored(r)-goalsConceded(r)) 
+		}
 	}
 }
