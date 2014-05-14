624969a7d0390be5e4741768d29492b4cd71eabb
Friðrik Runólfsson <friddz@gmail.com>
2014-05-14 22:40:47 +0000

    fixes to sort-by-name implementation for teams

--- team.go
+++ team.go
@@ -10,20 +10,20 @@ func (t Team) Id() int {
 	return t.id
 }
 
-func NewTeam(teamId int, teamName string) *Team {
-	return &Team{teamId, teamName, make([]Result, 0, 0)}
+func NewTeam(teamId int, teamName string) Team {
+	return Team{teamId, teamName, make([]Result, 0, 0)}
 }
 
 type ByName []Team
 
 func (t ByName) Len() int {
-	return len(t.Name)
+	return len(t)
 }
 
-func (t ByName) Swap(i, j) {
+func (t ByName) Swap(i int, j int) {
 	t[i], t[j] = t[j], t[i]
 }
 
-func (t ByName) Less(i, j) {
+func (t ByName) Less(i int, j int) bool {
 	return t[i].Name < t[j].Name
 }
