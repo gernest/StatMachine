3a92ab1de879230a6c531a3aeacb067b45a2f0af
Friðrik Runólfsson <friddz@gmail.com>
2014-05-14 22:06:52 +0000

    added sort-by-name implementation for teams

--- team.go
+++ team.go
@@ -1,14 +1,29 @@
 package statmachine
 
-type Team struct{
-	id int
-	Name string
+type Team struct {
+	id      int
+	Name    string
 	Results []Result
 }
 
-func (t Team) Id() int{
+func (t Team) Id() int {
 	return t.id
 }
-func NewTeam(teamId int, teamName string) *Team{
-	return &Team{teamId, teamName, make([]Result, 0,0)}
+
+func NewTeam(teamId int, teamName string) *Team {
+	return &Team{teamId, teamName, make([]Result, 0, 0)}
+}
+
+type ByName []Team
+
+func (t ByName) Len() int {
+	return len(t.Name)
+}
+
+func (t ByName) Swap(i, j) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t ByName) Less(i, j) {
+	return t[i].Name < t[j].Name
 }
