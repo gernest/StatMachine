3fa4ada740d4fe460367de7d0577b91d8e994595
Friðrik Runólfsson <friddz@gmail.com>
2014-05-15 23:50:48 +0000

    Changed league back to contain a slice of pointers to teams

--- league.go
+++ league.go
@@ -4,18 +4,18 @@ import "errors"
 
 type League struct{
 	name string
-	Teams []Team
+	Teams []*Team
 }
 
 func NewLeague(name string) League{
-	return League{name, make([]Team, 0,0)}
+	return League{name, make([]*Team, 0,0)}
 }
 
 func FindTeamByName(league League, name string) (*Team, error){
 	
 	for _,t := range league.Teams{
 		if(t.Name == name){
-			return &t, nil
+			return t, nil
 		}
 	}
 	return nil, errors.New("Couldnt find a team with the give name")
