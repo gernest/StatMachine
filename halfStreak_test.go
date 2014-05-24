a1e2a3243f1af7ddaa54b850a39770e88511ab96
Friðrik Runólfsson <friddz@gmail.com>
2014-05-24 23:36:58 +0000

    Fixed broken unit test setup

--- halfStreak_test.go
+++ halfStreak_test.go
@@ -114,10 +114,10 @@ func TestHalfsScoredInInARowWhenOnlyOne(t *testing.T) {
 
 func TestHalfsConcededInInARow(t *testing.T) {
 	allResults := []Result{
+		Result{0, 0, 0, 3, 0, 1, true, 0, time.Now(), 1},		
 		Result{1, 0, 0, 3, 0, 2, true, 0, time.Now(), 1},
 		Result{2, 0, 0, 1, 0, 1, true, 0, time.Now(), 1},
 		Result{3, 0, 0, 2, 0, 1, true, 0, time.Now(), 1},
-		Result{0, 0, 0, 3, 0, 1, true, 0, time.Now(), 1},
 		Result{2, 0, 0, 3, 0, 2, true, 0, time.Now(), 1},
 		Result{3, 0, 0, 3, 0, 3, true, 0, time.Now(), 1},
 	}
