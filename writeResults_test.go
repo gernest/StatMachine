56f53a41e0238f653abcbeebb4768000b912378b
friddz <fridrikr@PC-FDIS-72-L.fd.local>
2014-09-28 22:14:23 +0000

    fixed calls to result constructor

--- writeResults_test.go
+++ writeResults_test.go
@@ -1,38 +1,38 @@
 package statmachine
 
 import (
-		"testing"
-		"time"
-		)
+	"testing"
+	"time"
+)
 
-func TestWriteResults(t *testing.T){
-	allResults  := []Result{
-		Result{0,0,1,0,0,0,true,0, time.Now(),1, CardInfo{}},
-		Result{1,0,2,0,0,0,false,0, time.Now(),1, CardInfo{}},
-		Result{2,0,0,3,0,0,true,0, time.Now(),1, CardInfo{}},
-		Result{3,0,1,1,0,0,false,0, time.Now(),1, CardInfo{}},
-		Result{4,0,3,1,0,0,false,0, time.Now(),1, CardInfo{}},
-		Result{5,0,1,1,0,0,true,0, time.Now(),1, CardInfo{}},
+func TestWriteResults(t *testing.T) {
+	allResults := []Result{
+		Result{0, 0, 1, 0, 0, 0, true, 0, time.Now(), 1, CardInfo{}, []GoalInfo{}},
+		Result{1, 0, 2, 0, 0, 0, false, 0, time.Now(), 1, CardInfo{}, []GoalInfo{}},
+		Result{2, 0, 0, 3, 0, 0, true, 0, time.Now(), 1, CardInfo{}, []GoalInfo{}},
+		Result{3, 0, 1, 1, 0, 0, false, 0, time.Now(), 1, CardInfo{}, []GoalInfo{}},
+		Result{4, 0, 3, 1, 0, 0, false, 0, time.Now(), 1, CardInfo{}, []GoalInfo{}},
+		Result{5, 0, 1, 1, 0, 0, true, 0, time.Now(), 1, CardInfo{}, []GoalInfo{}},
 	}
-	
+
 	desc := ResultInfoString(allResults)
-	if(desc!="Total (W-D-L): 3-2-1"){
+	if desc != "Total (W-D-L): 3-2-1" {
 		t.Errorf("Didnt get correct result set string, got %v", desc)
 	}
 }
 
-func TestWriteSequenceString(t *testing.T){
-	allResults  := []Result{
-		Result{0,0,1,0,0,0,true,0, time.Now(),1, CardInfo{}},
-		Result{1,0,2,0,0,0,false,0, time.Now(),1, CardInfo{}},
-		Result{2,0,0,3,0,0,true,0, time.Now(),1, CardInfo{}},
-		Result{3,0,1,1,0,0,false,0, time.Now(),1, CardInfo{}},
-		Result{4,0,3,1,0,0,false,0, time.Now(),1, CardInfo{}},
-		Result{5,0,1,1,0,0,true,0, time.Now(),1, CardInfo{}},
+func TestWriteSequenceString(t *testing.T) {
+	allResults := []Result{
+		Result{0, 0, 1, 0, 0, 0, true, 0, time.Now(), 1, CardInfo{}, []GoalInfo{}},
+		Result{1, 0, 2, 0, 0, 0, false, 0, time.Now(), 1, CardInfo{}, []GoalInfo{}},
+		Result{2, 0, 0, 3, 0, 0, true, 0, time.Now(), 1, CardInfo{}, []GoalInfo{}},
+		Result{3, 0, 1, 1, 0, 0, false, 0, time.Now(), 1, CardInfo{}, []GoalInfo{}},
+		Result{4, 0, 3, 1, 0, 0, false, 0, time.Now(), 1, CardInfo{}, []GoalInfo{}},
+		Result{5, 0, 1, 1, 0, 0, true, 0, time.Now(), 1, CardInfo{}, []GoalInfo{}},
 	}
-	
+
 	desc := ResultSequenceString(allResults)
-	if(desc!="WWLDWD"){
+	if desc != "WWLDWD" {
 		t.Errorf("Didnt get correct result sequence string, got %v", desc)
 	}
 
